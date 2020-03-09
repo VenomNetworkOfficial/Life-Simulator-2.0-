6c17639a86de89eaf4ca0e28b728bf61efa62146
VenomNetworkOfficial <61607948+VenomNetworkOfficial@users.noreply.github.com>
2020-03-09 12:20:06 -0400

    Update 2.0
    This Update Is Just Some More Programming.

---
+++ Life Simulator.py
@@ -0,0 +1,131 @@
+print("Welcome To Life Simulator Python Edition (3.7.6)")
+print("Created By Keegan Meddaugh")
+print("Enter Your Characters Name")
+username = input()
+print("Your Name Is, " + username)
+print("Press Enter Key To Start")
+input()
+print("Age 1:")
+print("You Want To Eat Carrots And Peas,")
+print("Not Spinach And Carrots")
+print("What Do You Do?")
+print("1. Eat Spinach And Carrot Soup")
+print("2. Yell At Mom")
+answer1 = input()
+if answer1 == "1":
+    print("You ate the Spinach & Carrot Soup")
+if answer1 == "2":
+    print("You Yelled At Mom. You Got Grounded From Seeing Your Friends At Daycare.")
+print("Age 2:")
+print("You Need To Go To The Bathroom, But Mommy Isnt Near.")
+print("What Do You Do?")
+print("1. Yell For Mom")
+print("2. Poop Pants")
+answer2 = input()
+if answer2 == "2":
+    print("You Pooped Your Pants And It Smelled Like Chicken.")
+if answer2 == "1":
+    print("You Yelled For Mom, She Was In The Bathroom. You Pooped In Your Pants.")
+print("Age 3:")
+print("At Daycare Your Friend Steven Got Curious And Asked To See Your Privates")
+print("What Do You Do?")
+print("1. Let Him See Your Privates")
+print("2. Say No")
+print("3. Tell On Him")
+answer4 = input()
+if answer4 == "1":
+    print("You Let Him See Your Privates. You Guys Had Some Fun ;)")
+if answer4 == "2":
+    print("You Told Him No. He Was Sad.")
+if answer4 == "3":
+    print("You Told On Him. He Was Sent To The Office")
+print("Age 4:")
+print("Your Mom Went To The Store And She Left You Home Alone")
+print("What Do You Do?")
+print("1. Leave Home")
+print("2. Wait For Mom")
+answer5 = input()
+if answer5 == "1":
+    print("You Left Home. You Mom Got Mad At You Because It Took A While To Find You")
+if answer5 == "2":
+    print("You Waited For Mom")
+    print("She Came Home With Cookies")
+print("Thanks For Playing The Demo!")
+print("Insert Password For Demo Override")
+password = input()
+if password == "0025":
+    print("Demo Override Succeeded")
+    print("Age 5:")
+    print("You Were Told How Babies Are Made")
+    print("How Do You React?")
+    print("1. OMG!")
+    print("2. Eh. Ok.")
+    answer6 = input()
+    if answer6 == "1":
+        print("You Freaked Out")
+    if answer6 == "2":
+        print("You Didnt Freak Out. Your Parent Were Suprised")
+    print("Age 6:")
+    print("You Said A Swear Word, And Your Mom Got Angry")
+    print("What Do You Do?")
+    print("1. Swear More")
+    print("2. Apologize")
+    answer7 = input()
+    if answer7 == "1":
+        print("You Swore More. You Got Grounded")
+    if answer7 == "2":
+        print("You Apologized. She Accepted")
+    print("Age 7:")
+    print("Your Mom Was Scolding You About Talking To People On The Internet.")
+    print("What Do You Do?")
+    print("1. Stop Talking To Random People On The Internet")
+    print("2. Continue Talking To Random People On The Internet")
+    answer8 = input()
+    if answer8 == "1":
+        print("You Stopped Talking To People Who Aparrently Seemed To Be In Contact With The KKK")
+    if answer8 == "2":
+        print("You Got Yourself Situated With The KKK")
+        print("Achivement Earned:")
+        print("'Who Said I Was A Terrorist?'")
+    print("Age 8:")
+    print("Your Mom Asked If You Were With Your Drug-Addict Uncle")
+    print("How Do You Respond")
+    print("1. Say You Were")
+    print("2. Say You Were Not")
+    answer9 = input()
+    if answer9 == "1":
+        print("You Mom Told You Not To Be In Contact With Him")
+    if answer9 == "2":
+        print("You Said You Were Not. She Found Out You Were Lying, You Got Grounded")
+    print("Age 9:")
+    print("Your Mom Asked You What You Wanted For Your 10th Birthday")
+    print("What Do You Want?")
+    print("1. GameStation")
+    print("2. Racecar")
+    answer10 = input()
+    if answer10 == "1":
+        print("You Asked For A Gamestation")
+    if answer10 == "2":
+        print("You Asked For A Racecar")
+    print("Age 10:")
+    print("ITS YOUR 10TH BIRTHDAY!")
+    print("What Do You Do?")
+    print("1. Open Presents")
+    answer11 = input()
+    if answer11 == "1":
+        if answer10 == "2":
+            print("You Got A Racecar")
+        if answer10 == "1":
+            print("You Got A Gamestation")
+    print("Age 11:")
+    print("You Feel More Adult Now, What Do You Do?")
+    print("1. Masturbate")
+    print("2. Drink Wine")
+    print("3. Play With Toys")
+    answer12 = input()
+    if answer12 == "1":
+        print("You Masturbated With You Tiny PP")
+    if answer12 == "2":
+        print("You Snuck Some White Wine Back To Your Room, It Wasnt Wine At All")
+    if answer12 == "3":
+        print("You Figured Playing With Toys Was Irrelivant")
